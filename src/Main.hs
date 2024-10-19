50db57fd7ce090a2e0c57b3720542a33b6e4c7a6
kubni <nikola.kuburovic123@gmail.com>
2024-10-19 12:10:22 +0200

    Define Expr data type and its string equivalent

--- src/Main.hs
+++ src/Main.hs
@@ -1,4 +1,39 @@
 module Main where
 
+type Name = String
+
+-- TODO: GADTs
+-- data Expr where
+--   TmVar :: VarName -> Expr
+--   TmAbs :: VarName -> Expr -> Expr
+--   TmApp :: Expr -> Expr -> Expr
+--   deriving (Eq, Show)
+
+
+-- Example abstraction: λx.x
+-- Example application: (λx.x) x
+
+data Expr = Var Name
+            | Abs Name Expr
+            | App Expr Expr
+            deriving (Eq, Show)
+
+encode :: Expr -> String
+encode = go where
+  go (Var name) = name
+  go (Abs name body) = "λ" ++ name ++ "." ++ go body
+  go (App expr1 expr2) = "(" ++ go expr1 ++ go expr2 ++ ")"
+
+
+
+
+
+
+
+
+
+
+
+
 main :: IO ()
 main = putStrLn "Hello, Haskell!"
